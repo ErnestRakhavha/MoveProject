f705ef9f72a3e7ff0dbd4c0a0e5897032e1b6b5d
Ernest <ernest@kazazoom.com>
2016-10-13 16:50:46 +0200

    added first version of the moveproject

---
+++ MoveProject/app/src/main/java/com/example/ernest/moveproject/MoveActivity.java
@@ -0,0 +1,141 @@
+package com.example.ernest.moveproject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MoveActivity extends AppCompatActivity {
+
+        private static final String TAG = "MainActivity" ;
+        private ImageView imageView1, imageView2, imageView3;
+        private float mLastTouchX, mLastTouchY;
+        private float posX;
+        private float posY;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_move);
+
+            imageView1 =(ImageView) findViewById(R.id.image1);
+            imageView2 = (ImageView) findViewById(R.id.image2);
+            imageView3 = (ImageView) findViewById(R.id.image3);
+
+            imageView1.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Log.e(TAG, "ImageView1: OnLongClickListener() method");
+                    View.DragShadowBuilder shadowBuilder =
+                            new View.DragShadowBuilder(v);
+
+                    v.startDrag(null, shadowBuilder,
+                            ((ImageView) v).getDrawable(), 0);
+
+                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(100);
+
+                    return true;
+                }
+            });
+
+
+            imageView2.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Log.e(TAG, "Imageview2: OnLongClickListener()");
+                    View.DragShadowBuilder shadowBuilder =
+                            new View.DragShadowBuilder(v);
+
+                    v.startDrag(null, shadowBuilder,
+                            ((ImageView) v).getDrawable(), 0);
+
+                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(100);
+                    return true;
+                }
+            });
+
+
+            imageView3.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+
+                    Log.e(TAG, "ImageView3: OnLongClickListener() ");
+                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+
+                    imageView3.startDrag(null, shadowBuilder, ((ImageView) v).getDrawable(), 0);
+
+                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(100);
+                    return true;
+                }
+            });
+
+
+       /* imageView1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                float x = 0;
+                float y = 0;
+
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        //get x and values
+                        x = event.getX();
+                        y = event.getY();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+
+                        posX = mLastTouchX - x;
+                        posY = mLastTouchY - y;
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        break;
+
+                    default:
+                        return false;
+
+
+                }
+                return true;
+            }
+        });
+     */
+        }
+
+   /* public ImageView switchImageView(ImageView v)
+    {
+
+        v.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                Log.e(TAG, "ImageView3: OnLongClickListener() ");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+
+                imageView3.startDrag(null, shadowBuilder, ((ImageView) v).getDrawable(), 0);
+
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(100);
+                return true;
+            }
+        });
+
+        return v;
+    }
+    */
+
+    }
---
+++ MoveProject/app/src/main/java/com/examples/ernest/DropTargetView.java
@@ -0,0 +1,112 @@
+package com.examples.ernest;
+
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by Ernest on 2016/10/13.
+ */
+public class DropTargetView extends ImageView implements View.OnDragListener {
+
+    private boolean mDropped;
+
+    public DropTargetView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DropTargetView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DropTargetView(Context context, AttributeSet attrs,
+                          int defaultStyle) {
+        super(context, attrs, defaultStyle);
+        init();
+    }
+
+    private void init() {
+        // set a valid listener to receive DragEvents
+        setOnDragListener(this);
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        PropertyValuesHolder pvhX, pvhY;
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                //React to a new drag by shrinking the view
+                pvhX = PropertyValuesHolder.ofFloat("scaleX", 0.5f);   //enums/lookup
+                pvhY = PropertyValuesHolder.ofFloat("scaleY", 0.5f);
+                ObjectAnimator.ofPropertyValuesHolder(this,
+                        pvhX, pvhY).start();
+                //Clear the current drop image on a new event
+                setImageDrawable(null);
+                mDropped = false;
+                break;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                // React to a drag ending by resetting the view size
+                // if we weren't the drop target.
+                if (!mDropped) {
+                    pvhX = PropertyValuesHolder.ofFloat("scaleX", 1f);
+                    pvhY = PropertyValuesHolder.ofFloat("scaleY", 1f);
+                    ObjectAnimator.ofPropertyValuesHolder(this,
+                            pvhX, pvhY).start();
+                    mDropped = false;
+                    break;
+                }
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                //React to a drag entering this view by growing slightly
+
+                pvhX = PropertyValuesHolder.ofFloat("scaleX", 0.75f);
+                pvhY = PropertyValuesHolder.ofFloat("scaleY", 0.75f);
+                ObjectAnimator.ofPropertyValuesHolder(this,
+                        pvhX, pvhY).start();
+                break;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                //React to a drag leaving by returning to previous size
+                pvhX = PropertyValuesHolder.ofFloat("scaleX", 0.5f);
+                pvhY = PropertyValuesHolder.ofFloat("scaleY", 0.5f);
+                ObjectAnimator.ofPropertyValuesHolder(this,
+                        pvhX, pvhY).start();
+                break;
+
+            case DragEvent.ACTION_DROP:
+
+                //This animation shrinks the view briefly down to nothing
+                // and then back.
+                Keyframe frame0 = Keyframe.ofFloat(0f, 0.75f);
+                Keyframe frame1 = Keyframe.ofFloat(0.5f, 0f);
+                Keyframe frame2 = Keyframe.ofFloat(1f, 0.5f);
+                pvhX = PropertyValuesHolder.ofKeyframe("scaleX",
+                        frame0, frame1, frame2);
+                pvhY = PropertyValuesHolder.ofKeyframe("scaleY",
+                        frame0, frame1, frame2);
+                ObjectAnimator.ofPropertyValuesHolder(this,
+                        pvhX, pvhY).start();
+                //Set our image from the Object passed with the DragEvent
+                setImageDrawable((Drawable) event.getLocalState());
+
+                mDropped = true;
+                break;
+            default:
+                //Ignore events we aren't interested in
+                return false;
+        }
+
+        return true;
+    }
+
+
+}
